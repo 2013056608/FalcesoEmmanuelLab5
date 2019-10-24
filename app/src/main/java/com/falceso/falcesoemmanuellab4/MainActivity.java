16d5fe6c0bd5323c4c214a4f5876b9ace1e28ad9
notEmmaan31 <47667262+notEmmaan31@users.noreply.github.com>
2019-10-24 23:53:14 +0800

    Version list

--- app/src/main/java/com/falceso/falcesoemmanuellab4/MainActivity.java
+++ app/src/main/java/com/falceso/falcesoemmanuellab4/MainActivity.java
@@ -1,8 +1,12 @@
 package com.falceso.falcesoemmanuellab4;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -25,16 +29,48 @@ public class MainActivity extends AppCompatActivity {
         String[] androidVer = getResources().getStringArray(R.array.android);
         LinearLayout mainLayout = findViewById(R.id.main_layout);
 
-        TextView text;
+
 
         for(int i = 0; i < androidVer.length; i++){
             LinearLayout currLayout = new LinearLayout(this);
-
+            TextView text = new TextView(this);
             ImageView img = new ImageView(this);
 
+            StringBuilder details = new StringBuilder();
+            final String[] detail = androidVer[i].split("!");
 
-            img.setImageResource(draw[1]);
+            Log.d("error",detail[3]);
+            final int iterator = i;
+            details.append("Cupcake");
+            details.append("\nVer. " + detail[1]);
+            details.append("\nAPI level " + detail[3]);
+            details.append("\nReleased " + detail[2]);
+            img.setImageResource(draw[i]);
+            currLayout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                    dialog.setTitle(detail[0]);
+                    dialog.setMessage(detail[4]);
+                    dialog.setIcon(draw[iterator]);
+                    dialog.setNeutralButton("CLOSE", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick (DialogInterface dialog,int which){
+                            dialog.dismiss();
+                        }
+                    });
+                    dialog.create().show();
+
+                }
+            });
+
+
+
+            text.setText(details.toString());
+
             currLayout.addView(img);
+            currLayout.addView(text);
+
             mainLayout.addView(currLayout);
 
         }
